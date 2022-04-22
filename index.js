6cc6c1143aa2e5951e6504033e1be865a4609c64
adityajai25 <adityajai243@gmail,com>
2022-04-22 12:45:32 +0530

    Score display was added

--- index.js
+++ index.js
@@ -95,8 +95,7 @@ function animate(){
 if (scoreoffset > 2000){
     console.log('You Win the game')
     window.alert('Congrats, You won the game')
-    let value = True
-    window.alert('Your score :+',+scoreoffset)
+    window.alert('Your score :'+scoreoffset)
 }}
 animate()
 addEventListener('keydown',({keyCode}) => {
